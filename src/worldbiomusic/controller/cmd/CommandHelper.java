68a86e9a4247d7999c8ea7863e76a67b8c6e4f89
worldbiomusic <worldbiomusic@gmail.com>
2020-04-19 22:16:38 +0900

    make static/dynamic choie gui in CommandHelper

--- src/worldbiomusic/controller/cmd/CommandHelper.java
+++ src/worldbiomusic/controller/cmd/CommandHelper.java
@@ -1,12 +1,24 @@
 package worldbiomusic.controller.cmd;
 
+import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+
+import worldbiomusic.controller.util.InventoryGUIHelper;
 
 public class CommandHelper implements CommandExecutor{
 
+	InventoryGUIHelper guiHelper;
+	
+	public CommandHelper()
+	{
+		this.guiHelper = new InventoryGUIHelper();
+	}
+	
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 		
@@ -31,7 +43,18 @@ public class CommandHelper implements CommandExecutor{
 		{
 			p.sendMessage("open controller GUI");
 			
+			// get Static / Dynamic choice inventory GUI
+			guiHelper.createNewInventory(null, 9);
+			guiHelper.setEmptySpaceToItem(Material.THIN_GLASS, " ");
 			
+			
+			guiHelper.setItem(3, new ItemStack(Material.ENDER_CHEST), "Static");
+			guiHelper.setItem(5, new ItemStack(Material.CHEST), "Dynamic");
+			
+			Inventory inv = guiHelper.getInventory();
+			
+			// open inv
+			p.openInventory(inv);
 			
 			return true;
 		}
--- src/worldbiomusic/controller/util/GUIHelper.java
+++
@@ -1,5 +0,0 @@
-package worldbiomusic.controller.util;
-
-public class GUIHelper {
-
-}
